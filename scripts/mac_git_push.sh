2f3768fd8ce98db88a4332b20de144d00f7a1d8f
Santosh Shedbalkar <sanoysyg@gmail.com>
2021-05-09 21:23:58 +0530

    Updated git_push script

--- scripts/mac_git_push.sh
+++ scripts/mac_git_push.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# cd ../
-
 git add .
 echo Enter Git commit message: 
 read message
@@ -9,6 +7,5 @@ message=`echo $message | sed -e 's/^[[:space:]]*//'`
 if [ ${#message} -eq 0 ]; then
     message=`date '+%A %d-%B, %Y - %T'`
 fi
-echo "Message: $message"
-git commit -m $message
+git commit -m "$message"
 git push
